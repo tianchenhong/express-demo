58fe65673d0c2aee7e9166ac63765bc363cd0b70
tianchenhong <1035939037@qq.com>
2018-06-06 17:20:34 +0800

    second

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('啦啦的天天')
 })
 
+app.get('/lalala', function (req, res) {
+  res.send('哈哈哈哈')
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
